2911665df7df29d2b73a30a05596482821ee7806
Olaleye Fuad <heniolarr@gmail.com>
2024-09-15 08:52:55 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Norman Block";
+  const tree = new MerkleTree(niceList)
+
+  const proof = tree.getProof(niceList.findIndex(n => n === name));
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    name, proof
+  });
+
+  console.log({ gift });
+}
+
+main();
